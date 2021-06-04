85225652c823668a6e1c300e3ecd7a87558caf02
Masatoshi Itagaki <masatoshi@rails.to>
2021-06-04 16:50:48 +0900

    add sh -c to echo redicrect

--- install_modules.sh
+++ install_modules.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # install gcsfuse
-sudo echo "deb http://packages.cloud.google.com/apt gcsfuse-bionic main" > /etc/apt/sources.list.d/gcsfuse.list
+sudo sh -c "echo 'deb http://packages.cloud.google.com/apt gcsfuse-bionic main' > /etc/apt/sources.list.d/gcsfuse.list"
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt update
 sudo apt install gcsfuse -y
