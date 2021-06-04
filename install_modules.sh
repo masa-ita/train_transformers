5311f78d8cd48540495f1a9296f8fa98ab75a725
Masatoshi Itagaki <masatoshi@rails.to>
2021-06-04 16:42:45 +0900

    sudo to install modules

--- install_modules.sh
+++ install_modules.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # install gcsfuse
-echo "deb http://packages.cloud.google.com/apt gcsfuse-bionic main" > /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-apt update
-apt install gcsfuse -y
+sudo echo "deb http://packages.cloud.google.com/apt gcsfuse-bionic main" > /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt update
+sudo apt install gcsfuse -y
 
 # install python libs
 pip install transformers datasets fugashi unidic-lite cloud-tpu-client
